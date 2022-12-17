fc860ff0b6ff5282b56856b94677a74d50c60914
harryzhu <harry.chuh@gmail.com>
2022-12-17 22:41:55 +0800

    -

--- cmd/cmdb3sum.go
+++ cmd/cmdb3sum.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +15,11 @@ var b3sumCmd = &cobra.Command{
 	Use:   "b3sum",
 	Short: "b3sum --input=your-local-file.txt [--output=if-you-want-to-save-hash-value-to-file.txt]",
 	Long:  ``,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if Input == "" {
+			log.Fatal("--input= cannot be empty")
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		p := Blake3SumFile(Input)
 		Colorintln("green", p)
---
+++ cmd/cmdsha256.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// sha256Cmd represents the sha256 command
+var sha256Cmd = &cobra.Command{
+	Use:   "sha256",
+	Short: "sha256 --input=your-local-file.txt [--output=if-you-want-to-save-hash-value-to-file.txt]",
+	Long:  `-`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if Input == "" {
+			log.Fatal("--input= cannot be empty")
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		Colorintln("green", "sha256 is running ...")
+		p := SHA256File(Input)
+		Colorintln("green", p)
+
+		if Output != "" {
+			SaveFile(Output, []byte(p))
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sha256Cmd)
+
+	rootCmd.MarkFlagRequired("input")
+}

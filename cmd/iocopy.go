fcbff090d012a374ea03242f3593b07fb3b1e3ce
harryzhu <harry.chuh@gmail.com>
2023-05-14 15:37:01 +0800

    -

--- cmd/iocopy.go
+++ cmd/iocopy.go
@@ -9,15 +9,13 @@ import (
 	"log"
 )
 
-func CopyFileWithBar(r io.Reader, w io.Writer, barMax64 int64) error {
-	var err error
+func CopyFileWithBar(r io.Reader, w io.Writer, barMax64 int64) (err error) {
 	if isBar {
 		bar64.WithMax64(barMax64)
 		_, err = io.Copy(io.MultiWriter(w, bar64), r)
 		bar64.Finish()
 	} else {
 		_, err = io.Copy(w, r)
-
 	}
 
 	if err != nil {
